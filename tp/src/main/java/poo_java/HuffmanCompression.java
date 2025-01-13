1e436c388490121f01391abca8a4aaf8dba5a9ca
Patrick Pastouret <pastouretpatrick@gmail.com>
2025-01-13 19:24:09 +0100

    Modification de la méthode buildHuffmanTree et de la classe Huffnode + avancement de la méthode compress

--- tp/src/main/java/poo_java/HuffmanCompression.java
+++ tp/src/main/java/poo_java/HuffmanCompression.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.Enumeration;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collections;
 
 public class HuffmanCompression implements Compression {
@@ -15,29 +16,67 @@ public class HuffmanCompression implements Compression {
     @Override
     public byte[] compress(String data) {
     // Implémentation de la compression
-        Hashtable ht = new Hashtable();
+        Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
         for (int i = 0; i < data.length(); i++) {
-            Integer n_occurrences = (Integer)ht.get(data.charAt(i));
+            Integer n_occurrences = ht.get(data.charAt(i));
             if (n_occurrences == null)
                 ht.put(data.charAt(i), 1);
             else
                 ht.put(data.charAt(i), n_occurrences + 1);
         }
-        for (Enumeration e = ht.keys(); e.hasMoreElements();) {
-            Character key = (Character) e.nextElement() ;
-            System.out.println(key + " : " + (Integer)ht.get(key));
+        
+
+        // Affichage de la table de fréquence
+        for (Enumeration<Character> e = ht.keys(); e.hasMoreElements();) {
+            Character key = e.nextElement() ;
+            System.out.println(key + " : " + ht.get(key));
         }
-        this.buildHuffmanTree(ht);
-        return null;
+
+        
+        // Génération de l'arbre de Huffman
+        HuffmanNode huffmanTree = this.buildHuffmanTree(ht);
+        generateCodes(huffmanTree, "");
+
+        BitSet compressedData = new BitSet();
+
+        char[] code; // tableau de bits d'un code 
+        for (int i = 0, j = 0; i < data.length(); i++, j += code.length) {
+            // Récupère le code binaire du caractère
+            System.err.println(this.charToCode.get('a'));
+            code = this.charToCode.get(data.charAt(i)).toCharArray(); 
+            for (int k = 0; k < code.length; k++) {
+                if (code[k] == '1')
+                    compressedData.set(j + k);
+            }
+        }
+
+
+        return compressedData.toByteArray();
     }
     @Override
     public String decompress(byte[] data) {
     // Implémentation de la décompression
         return null;
     }
-    // private void generateCodes(HuffmanNode root, String code) {
-    // // Récursion pour générer les codes binaires
-    // }
+    private void generateCodes(HuffmanNode node, String code) {
+
+        this.charToCode = new Hashtable<Character, String>();
+        this.codeToChar = new Hashtable<String, Character>();
+    // Génère les codes binaires pour chaque caractère
+        if (node.isLeaf()) {
+            this.charToCode.put(((ImmutablePair<Character, Integer>)node.getValue()).getKey(), code);
+            this.codeToChar.put(code, ((ImmutablePair<Character, Integer>)node.getValue()).getKey());
+            System.out.println("Code de " + ((ImmutablePair<Character, Integer>)node.getValue()).getKey() + " : " + code);
+            char test = ((ImmutablePair<Character, Integer>)node.getValue()).getKey();
+            System.out.println(this.charToCode.get(((ImmutablePair<Character, Integer>)node.getValue()).getKey()));
+            System.out.println(this.charToCode.get(test));
+            // TODO corriger le bug du code de 'a' qui retourne null
+        }
+        else {
+            this.generateCodes((HuffmanNode)node.getLeft(), code + "0");
+            this.generateCodes((HuffmanNode)node.getRight(), code + "1");
+        }
+    }
     private HuffmanNode buildHuffmanTree(Hashtable<Character, Integer> frequencyTable) {
     // Construction de l’arbre de Huffman
         HuffmanNode newNode = null;
@@ -51,9 +90,9 @@ public class HuffmanCompression implements Compression {
         while (nodes.size() != 1) {
             Collections.sort(nodes);
             ImmutablePair<Character, Integer> pair = new ImmutablePair<Character, Integer>(null, 
-                (Integer)((ImmutablePair<Character, Integer>)nodes.get(0).getValue()).getValue() + 
-                (Integer)((ImmutablePair<Character, Integer>)nodes.get(1).getValue()).getValue());
-            newNode = new HuffmanNode((Integer)nodes.get(0).getValue() + (Integer)nodes.get(1).getValue());
+                ((ImmutablePair<Character, Integer>)nodes.get(0).getValue()).getValue() + 
+                ((ImmutablePair<Character, Integer>)nodes.get(1).getValue()).getValue());
+            newNode = new HuffmanNode(pair);
             newNode.setLeft(nodes.get(0));
             newNode.setRight(nodes.get(1));
             nodes.remove(0);
