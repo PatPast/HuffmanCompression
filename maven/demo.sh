6431f6bfc55f9d9fd2bce9e50274c864a988c400
Boulanger Matthieu <matthieu.boulanger.etu@univ-lemans.fr>
2025-02-04 11:38:16 +0100

    màj du script de démonstration

--- maven/demo.sh
+++ maven/demo.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # gestion des parametres : le premier est un fichier d'entree, le second est un fichier de sortie
-if [ $# -ne 2 ]; then
+if [ "$1" = '-h' -o "$1" = --help ]; then
     echo "Usage: $0 <fichier_entree> <fichier_sortie>"
     exit 1
+fi
+
+if [ $# -ne 2 ]; then
+    file_in=../données/dictionnaire_utf8.fich
+    file_out=../données/compressed.bin
+else
+    file_in="$1"
+    file_out="$2"
 fi
 
 
@@ -15,11 +23,20 @@ mvn clean compile package install
 echo "Running compression... press any key to continue"
 read -n 1 -s
 
-time java -jar "./target/tp-1.0-SNAPSHOT.jar" --compress -i $1 -o $2
+time java -jar "./target/maven-1.0-SNAPSHOT.jar" --compress -i "$file_in" -o "$file_out"
 
 echo "Running decompression... press any key to continue"
 read -n 1 -s
 
-time java -jar "./target/tp-1.0-SNAPSHOT.jar" --decompress -i $2 -o $2.decompressed
+time java -jar "./target/maven-1.0-SNAPSHOT.jar" --decompress -i "$file_out" -o "$file_out.decompressed"
 
-du -b $1 $2 $2.decompressed
+echo "File sizes:"
+du -b "$file_in" "$file_out" "$file_out.decompressed"
+
+echo "Comparing $file_in and $file_out.decompressed:"
+if cmp "$file_in" "$file_out.decompressed"
+then
+    echo "Same content"
+else
+    echo "Different content"
+fi
